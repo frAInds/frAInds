5e034d49957afc7bbc443a70472a4be99f335c1e
Jehee Cheon <jeheecheon@gmail.com>
2024-01-31 10:39:45 +0900

    dark mode

--- Projects/fe/src/pages/root/page/components/Header.jsx
+++ Projects/fe/src/pages/root/page/components/Header.jsx
@@ -1,6 +1,50 @@
+import FriendsLogo from "@/common/components/FriendsLogo";
+import { Switch } from "@/common/components/ui/switch";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 export const Header = () => {
-  return (
-    <div>Header</div>
+  const [isDarkModeOff, setIsDarkModeOff] = useState(false);
+
+  const handleChangeTheme = () => {
+    setIsDarkModeOff(!isDarkModeOff);
+    if (isDarkModeOff === false) {
+      localStorage.theme = 'light'
+      document.documentElement.classList.remove('dark');
+    }
+    else {
+      localStorage.theme = 'dark'
+      document.documentElement.classList.add('dark');
+    }
+  }
+
+  useEffect(() => {
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      document.documentElement.classList.add('dark');
+      setIsDarkModeOff(false);
+    }
+    else {
+      document.documentElement.classList.remove('dark');
+      setIsDarkModeOff(true);
+    }
+  }, []);
+
+  return (<>
+    <div className="fixed w-screen flex flex-row h-[70px] items-center bg-white 
+    shadow-lg px-5 dark:bg-slate-700  dark:drop-shadow-xl">
+      <FriendsLogo />
+      <Link>Button 1</Link>
+      <Link>Button 2</Link>
+      <Link>Button 3</Link>
+      <Switch
+        checked={isDarkModeOff}
+        onCheckedChange={handleChangeTheme}
+      />
+    </div>
+
+    <div className="h-[70px]"></div>
+
+  </>
   )
 }
 
--- Projects/fe/src/pages/root/page/index.jsx
+++ Projects/fe/src/pages/root/page/index.jsx
@@ -1,7 +1,38 @@
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/common/components/ui/carousel";
 
 export const Root = () => {
   return (
-    <div>Root</div>
+    <div className="dark:bg-slate-800 h-screen">
+
+      <div className="scroll-m-[30px] gap-[6vw] snap-mandatory snap-x flex overflow-x-auto pb-6
+      pt-[10vh]">
+        <div className="ml-[50vw] snap-center shrink-0">
+          <img className="w-[80vw]" src="https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
+        </div>
+        <div className="snap-center	shrink-0">
+          <img className="w-[80vw]" src="https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
+        </div>
+        <div className="snap-center	shrink-0">
+          <img className="w-[80vw]" src="https://images.unsplash.com/photo-1622890806166-111d7f6c7c97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
+        </div>
+        <div className="snap-center	shrink-0">
+          <img className="w-[80vw]" src="https://images.unsplash.com/photo-1590523277543-a94d2e4eb00b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
+        </div>
+        <div className="snap-center	shrink-0">
+          <img className="w-[80vw]" src="https://images.unsplash.com/photo-1575424909138-46b05e5919ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
+        </div>
+
+        <Carousel>
+          <CarouselContent>
+            <CarouselItem>
+              <img className="w-[80vw]" src="https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
+    </div>
   )
 }
 
