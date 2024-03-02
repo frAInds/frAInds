7ba92fc5e194da55f4b5260ba7a3a5309b5ea9bd
justwaterplz <102442975+justwaterplz@users.noreply.github.com>
2024-03-02 20:34:56 +0900

    created UsageModal.jsx
    사용방법 클릭 시 사용설명서 화면에 나옴.
    반투명 검정색 배경 + 검은색 화면에 흰색 텍스트
    사이트 테마에 맞게 변경되는건 추후 추가될예정.

--- Projects/fe/src/pages/root/page/components/Header.jsx
+++ Projects/fe/src/pages/root/page/components/Header.jsx
@@ -1,18 +1,24 @@
 import FriendsLogo from "@/common/components/FriendsLogo";
 import { Switch } from "@/common/components/ui/switch";
-import SignIn from "@/pages/account/sign-in/page";
+// import SignIn from "@/pages/account/sign-in/page";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import UsageModal from "./UsageModal";
 
 export const Header = () => {
+  //다크모드, 라이트모드
   const [isDarkModeOn, setIsDarkModeOn] = useState(false);
+
+  //사용방법 클릭 시 나오는 반투명 검정색 창 + 흰색 텍스트
+  const [isUsageOn, setIsUsageOn] = useState(false);
 
   //로그인 여부 가져오기
   const isAuthenticated = useSelector(
     (state) => state.user.isAuthenticated
   );
 
+  //테마변경
   const handleChangeTheme = () => {
     setIsDarkModeOn(!isDarkModeOn);
     if (isDarkModeOn === true) {
@@ -23,6 +29,19 @@ export const Header = () => {
       localStorage.theme = 'dark'
       document.documentElement.classList.add('dark');
     }
+  }
+
+  //UsageModal click handler
+  const handleUsageModal = () => {
+    // console.log('usage modal true');
+    setIsUsageOn(true);
+  }
+
+  //UsageModal close시
+  const handleCloseUsageModal = () => {
+    console.log('usage modal false');
+
+    setIsUsageOn(false);
   }
 
   useEffect(() => {
@@ -39,14 +58,16 @@ export const Header = () => {
   return (<>
     <div className="fixed w-screen flex flex-row h-[70px] items-center z-10 shadow-lg px-8
       justify-between
-     dark:bg-slate-950 bg-white  dark:drop-shadow-xl">
+    dark:bg-slate-950 bg-white  dark:drop-shadow-xl">
       <FriendsLogo className="mb-1"/>
       <div className="flex gap-5 text-indigo-400 font-bold text-xl">
 
         {/* //로그인 여부에 따라 다른 화면 보임 */}
         { isAuthenticated && <p>welcome user</p>}
         <Link to={ "/account/sign-in" }>로그인</Link>
-        <Link className="mr-10">사용방법</Link>
+        
+        {/* 다른 링크로 가는게 아니라서 div로 수정 + cursor올렸을 때 포인터로 바뀌게 수정함 */}
+        <div className="mr-10 cursor-pointer" onClick={handleUsageModal} role="button">사용방법</div>
         {isDarkModeOn ?
           <svg className="dark:fill-indigo-400" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Z" /></svg>
           : <svg className="fill-indigo-400" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M480-280q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Z" /></svg>
@@ -60,6 +81,9 @@ export const Header = () => {
     </div>
 
     <div className="h-[70px] w-full"></div>
+
+    {/* UsageModal의 활성/비활성 상태에 따른 창 */}
+    {isUsageOn && <UsageModal onClose={handleCloseUsageModal} />}
   </>
   )
 }
---
+++ Projects/fe/src/pages/root/page/components/UsageModal.jsx
@@ -0,0 +1,37 @@
+// header의 사용방법 눌렀을 때 나오는 반투명 검은색 사용설명서
+const UsageModal = ({ onClose }) => {
+    return (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+            <div className="bg-black text-white p-5 rounded-lg w-2/5 h-[600px] mx-auto">
+                <div className="flex flex-col min-h-full text-center">
+                    <p className="text-center text-3xl mb-12">사용설명서</p>
+                    <p className="mb-6">
+                        1. 캐릭터를 선택해주세요.
+                    </p>
+                    <p className="">
+                        2. 각 캐릭터는 고유 능력이 있습니다.
+                    </p>
+                    <p className="mb-6">
+                        캐릭터에게 질문해보세요.
+                    </p>
+                    <p className="mb-6">
+                        3. 유태민 죽어
+                    </p>
+                    <p className="">
+                        4. 건의사항 및 캐릭터 추가는                  
+                    </p>
+                    <p>
+                        제작자에게 문의해주세요.  
+                    </p>
+                </div>
+
+                <button
+                onClick={onClose}
+                className="absolute bottom-16 left-1/2 transform -translate-x-1/2 py-2 px-4 bg-red-500 hover:bg-red-800 text-white font-bold rounded">
+                    X
+                </button>
+            </div>
+        </div>
+    );
+};
+export default UsageModal;
