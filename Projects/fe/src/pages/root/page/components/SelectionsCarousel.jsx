f14a493f793570e67103073cc914ef0770a24c8a
yoonki <whdbsrl99@gmail.com>
2024-03-30 03:51:25 +0900

    added carousel

--- Projects/fe/src/pages/root/page/components/SelectionsCarousel.jsx
+++ Projects/fe/src/pages/root/page/components/SelectionsCarousel.jsx
@@ -1,25 +1,30 @@
 import { useEffect } from "react";
 import useEmblaCarousel from 'embla-carousel-react';
-import { WheelGesturesPlugin } from 'embla-carousel-wheel-gestures';
-
-import Autoplay from 'embla-carousel-autoplay';
-import exImg from '@/pages/test/page/images/1701826553654.jpg';
-import NavigationButtons from "./NavigationButtons";
+import {
+    SelectedSnapDisplay,
+    useSelectedSnapDisplay
+  } from './EmblaCarouselSelectedSnapDisplay'
+import {
+    PrevButton,
+    NextButton,
+    usePrevNextButtons
+  } from './EmblaCarouselArrowButtons'
 
 import { Link } from "react-router-dom";
 
-//carousel 형식으로 된 캐릭터 선택창
-const SelectionsCarousel = () => {
-    const [emblaRef, emblaApi] = useEmblaCarousel({
-        loop: true,
-        startIndex: 1
-    }, [
-        // Autoplay({
-        //     delay: 2000
-        // }),
-        WheelGesturesPlugin(),
-        // AutoHeight()
-    ])
+//carousel 형식으로 된 캐릭터 선택창 index.jsx에서 넘긴 값 그대로 받기
+const SelectionsCarousel = (props) => {
+    const {items, options, slides} = props;
+    const [emblaRef, emblaApi] = useEmblaCarousel(options);
+
+    const {
+        prevBtnDisabled,
+        nextBtnDisabled,
+        onPrevButtonClick,
+        onNextButtonClick
+    } = usePrevNextButtons(emblaApi)
+
+    const { selectedSnap, snapCount } = useSelectedSnapDisplay(emblaApi)
 
     useEffect(() => {
         if (emblaApi) console.log(emblaApi.slideNodes())
@@ -27,93 +32,40 @@ const SelectionsCarousel = () => {
 
     return (
         <>
-            <div className="overflow-hidden mt-10" ref={emblaRef}>
-                <div className="flex items-start">
+            <div className="overflow-hidden flex-grow">
+                {/* choose your character */}
+                <div className="flex mt-10 text-6xl text-center mb-20 text-indigo-400">
+                    {/* font 추후 수정 예정 */}
+                    <p>[ CHOOSE YOUR CHARACTER ]</p>
+                </div>
 
-                    {/* 첫 번째 셀렉션 */}
-                    <div className="ml-[10vw] shrink-0 grow-0 flex flex-col items-center gap-6">
-                        <img className=" bg-top  rounded-2xl w-[80vw] h-[45vh] block
-                            cursor-pointer"
-                            style={{
-                                backgroundImage: `url(${exImg})`
-                            }}
-                        />
-                        <NavigationButtons
-                            onLeftClicked={() => emblaApi.scrollPrev()}
-                            onRightClicked={() => emblaApi.scrollNext()}
-                        />
-                        <div className="shadow-2xl bg-indigo-100 dark:shadow-black dark:bg-slate-800 rounded-2xl 
-                            shrink-0 grow-0 w-full py-3 px-10 flex flex-col max-w-[780px] mt-10 text-pretty dark:text-indigo-400 dark:font-medium text-lg">
-                            <span className="m-auto font-extrabold text-4xl">
-                                유태민 나무위키
-                            </span>
-                            <br />
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                        </div>
-                    </div>
-
-                    {/* 두 번째 셀렉션 */}
-                    <div className="ml-[5vw] shrink-0 grow-0 flex flex-col items-center gap-6">
-                        <img className=" bg-top rounded-2xl w-[80vw] h-[45vh] block
-                            cursor-pointer"
-                            style={{
-                                backgroundImage: `url(${exImg})`
-                            }}
-                        />
-                        <NavigationButtons
-                            onLeftClicked={() => emblaApi.scrollPrev()}
-                            onRightClicked={() => emblaApi.scrollNext()}
-                        />
-                        <div className="shadow-2xl bg-indigo-100 dark:shadow-black dark:bg-slate-800 rounded-2xl 
-                            shrink-0 grow-0 w-full py-3 px-10 flex flex-col max-w-[780px] mt-10 text-pretty dark:text-indigo-400 dark:font-medium text-lg">
-                            <span className="m-auto font-extrabold text-4xl">
-                                충기 나무위키
-                            </span>
-                            <br />
-                            <p>이충기 1979년 서울대학교 계산통계학과(이학사)</p> 
-                            <p>1981년 서울대학교 계산통계학과(이학석사)</p>
-                            <p>1993년 조지아공과대학교 전산과학대학(전산학박사)</p>
-                            <p>1994 ~ 1996 한국전산원 전산망 표준본부 선임연구원</p>
-                            <p>2000 ~ 2001 명지대학교 정보통신교육연구센터 센터장</p>
-                            <p>2003 ~ 2004 조지아공과대학교 전산과학대학 방문교수</p>
-                            <p>2010 ~ 2011 퍼듀대학교 컴퓨터과학과 방문교수 </p>
-                            <p>1996 ~ 현재 명지대학교 컴퓨터공학과 교수 관심분야 | 모바일 컴퓨팅, 공학 교육</p>
-                        </div>
-                    </div>
-
-                    {/* 세 번째 셀렉션 */}
-                    <div className="ml-[5vw] mr-[10vw] shrink-0 grow-0 flex flex-col items-center gap-6">
-                        <Link to={"/chat/dog"}> 
-                            <img className="bg-top  rounded-2xl w-[80vw] h-[45vh] block
-                                cursor-pointer"
-                                style={{
-                                    backgroundImage: `url(${exImg})`
-                                }}
-                                onClick={() => console.log("dog Clicked")}
-
-                            />
-                        </Link>
-                        
-                        <NavigationButtons
-                            onLeftClicked={() => emblaApi.scrollPrev()}
-                            onRightClicked={() => emblaApi.scrollNext()}
-                        />
-                        <div className="shadow-2xl bg-indigo-100 dark:shadow-black dark:bg-slate-800 rounded-2xl 
-                            shrink-0 grow-0 w-full py-3 px-10 flex flex-col max-w-[780px] mt-10 text-pretty dark:text-indigo-400 dark:font-medium text-lg">
-                            <span className="m-auto font-extrabold text-4xl">
-                                강아지 나무위키
-                            </span>
-                            <br />
-                            왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈
-                            왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈
-                            왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈왈
+                <div className="embla">
+                    <div className="embla__viewport" ref={emblaRef}>
+                        <div className="embla__container">
+                            {items.map((image, index) => (
+                                <div className="embla__slide " key={index}>
+                                    
+                                    <Link to='/'>
+                                        <img className="slides_images rounded-lg shadow-2xl" src={image} alt="123" />
+                                    </Link>
+                                </div>
+                            ))}
                         </div>
                     </div>
                 </div>
+
+                <div 
+                className="embla__controls items-center gap-[1.2rem] mt-[1.0rem] justify-center flex">
+                    <div className="embla__buttons">
+                        <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+                        <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+                    </div>
+
+                    <SelectedSnapDisplay className="embla__snap__display"
+                    selectedSnap={selectedSnap}
+                    snapCount={snapCount}
+                    />
+                </div>                    
             </div>
         </>
 
--- Projects/fe/src/pages/root/page/index.jsx
+++ Projects/fe/src/pages/root/page/index.jsx
@@ -1,12 +1,27 @@
 
 import SelectionsCarousel from "./components/SelectionsCarousel";
 
+//images
+import exImg from '@/pages/test/page/images/1701826553654.jpg';
+import exImg1 from '@/pages/test/page/images/choongi_pic.png';
+import exImg2 from '@/pages/test/page/images/test.gif';
+
 export const Root = () => {
+
+  const OPTIONS = { loop: true, dragFree: true, align: "center", dragThreshold: 20}
+  const SLIDE_COUNT = 3;
+  const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
   return (
     <div className="dark:bg-slate-900 max-w-[100vw] min-h-screen
     flex flex-col items-center">
 
-      <SelectionsCarousel />
+
+      
+      <SelectionsCarousel 
+      items={[
+        exImg, exImg1, exImg2
+      ]} options={OPTIONS}
+      slides={SLIDES}/>
 
     </div>
   )
