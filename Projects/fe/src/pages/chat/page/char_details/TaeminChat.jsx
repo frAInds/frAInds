0bc8c2cc02913f94c6b67fabb071abb8c3bb7661
yoonki <whdbsrl99@gmail.com>
2024-04-12 13:37:39 +0900

    updated TaeminChat.jsx
    deleted middle-page,
    edited panel initial size to 45 55

--- Projects/fe/src/pages/chat/page/char_details/TaeminChat.jsx
+++ Projects/fe/src/pages/chat/page/char_details/TaeminChat.jsx
@@ -90,11 +90,10 @@ const TaeminChat = () => {
             if (messagesToSend.text.startsWith('```') && messagesToSend.text.endsWith('```')) {
                 const codeBlock = messagesToSend.text.slice(3, -3);
 
-                const width = 500;
-                const height = 500;
+                const width = 750;
+                const height = 750;
 
-                // Calculate the position of the new window
-                // const left = (window.innerWidth - width) / 2 + 50;
+                //팝업 화면을 중앙에 띄우려면 width, height를 계산해야 한다..
                 const left = (window.screen.width - width) / 2;
                 const top = (window.screen.height - height) / 2;
 
@@ -138,7 +137,7 @@ const TaeminChat = () => {
             <div className='flex w-full h-[92vh]' >
 
                 {/* 왼쪽 */}
-                <ResizablePanel defaultSize={30} order={1}>
+                <ResizablePanel defaultSize={55} order={1}>
                     <div className="flex w-full h-full flex-col bg-gray-200 dark:bg-testBlack">
                         {/* 3d goes here */}
                         <Canvas camera={{ position: [0,0,3] }}
@@ -153,24 +152,8 @@ const TaeminChat = () => {
                 {/* handle */}
                 <ResizableHandle withHandle style={{ backgroundColor: '#000080' }} className='dark:bg-white '/>
 
-                {/* 확장 화면: 초기엔 안보임 */}
-                {isBlockOpen && (
-                    <>
-                        <ResizablePanel defaultSize={35} order={2} collapsible={true}>
-                            <div className="flex w-full h-full flex-col opacity-50 dark:bg-testBlack
-                            transition-height duration-600 ease-in-out">
-
-                            </div>
-                        </ResizablePanel>
-                        {/* <ResizableHandle withHandle style={{ backgroundColor: '#000080' }} className='dark:bg-white '/> */}
-                </>
-                )}
-
-
-
-
                 {/* 오른쪽 */}
-                <ResizablePanel defaultSize={35} order={3}>
+                <ResizablePanel defaultSize={45} order={3}>
                     {/* <ResizablePanelGroup direction="vertical"> */}
                         <div className="flex h-full flex-col bg-gray-200 justify-between dark:bg-testBlack">
                             <div ref={chatContainerRef} className='flex flex-col-reverse overflow-auto '>
@@ -187,25 +170,6 @@ const TaeminChat = () => {
 
 
                             <div className="flex items-center justify-center">
-
-                                <TooltipProvider>
-                                    <Tooltip>
-                                        <TooltipTrigger asChild>
-                                            <button onClick={handleBlockOpen}>
-                                                <svg width="40px" height="40px" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
-                                                className='dark:fill-indigo-400 fill-indigo-400 ml-2 cursor-pointer'>
-                                                    <path d="M3 11C3 7.22876 3 5.34315 4.17157 4.17157C5.34315 3 7.22876 3 11 3H13C16.7712 3 18.6569 3 19.8284 4.17157C21 5.34315 21 7.22876 21 11V13C21 16.7712 21 18.6569 19.8284 19.8284C18.6569 21 16.7712 21 13 21H11C7.22876 21 5.34315 21 4.17157 19.8284C3 18.6569 3 16.7712 3 13V11Z" />
-                                                    <path d="M12 8L12 16" stroke="#33363F" strokeWidth="2" strokeLinecap="square" strokeLinejoin="round"/>
-                                                    <path d="M16 12L8 12" stroke="#33363F" strokeWidth="2" strokeLinecap="square" strokeLinejoin="round"/>
-                                                </svg>
-                                            </button>
-                                        </TooltipTrigger>
-                                        <TooltipContent>
-                                            <p>코드 창 확장하기</p>
-                                        </TooltipContent>
-                                    </Tooltip>
-                                </TooltipProvider>
-
                                 <form onSubmit={handleSendMessage} className='p-4 px-2 mt-auto flex w-95p mr-4'>
                                     <input
                                         type="text"
