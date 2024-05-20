9ca6b57c20b46a76e85f8d5ff2ca45608fe73755
justwaterplz <102442975+justwaterplz@users.noreply.github.com>
2024-05-20 17:18:18 +0900

    updated sign-in

--- Projects/fe/src/pages/account/sign-up/page/index.jsx
+++ Projects/fe/src/pages/account/sign-up/page/index.jsx
@@ -1,24 +1,59 @@
 import FriendsLogo from "@/common/components/FriendsLogo";
 import "@/pages/account/sign-up/css/sign-up.css";
-import { useState, useMemo } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import { Input } from "@nextui-org/react";
 
 
 const SignUp = () => {
-  // const [value, setValue] = useState("");
- 
+  // username, password, 확인용 password
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
+  //검증
+  const [isUsernameInvalid, setIsUsernameInvalid] = useState(false);
+  const [isPasswordInvalid, setIsPasswordInvalid] = useState(false);
+  const [isPasswordMismatch, setIsPasswordMismatch] = useState(false);
+  
   const navigate = useNavigate();
+
+  //username, password regex
+  const validateUsername = (value) => /^(?=.*[a-z])(?=.*[0-9])[a-z0-9]{10,20}$/.test(value);
+  const validatePassword = (value) => /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(value);
+
+  // // username 검증.
+  // const isInvalid = useMemo(() => {
+  //   if (username === "") return false;
+  //   return validateUsername(username) ? false : true;
+  // }, [username]);
+
+  const handleUsernameChange = (e) => {
+    const value = e.target.value;
+    setUsername(value);
+
+    console.log(`isInvalid`+isUsernameInvalid);
+  };
+
+  const handlePasswordChange = (e) => {
+    const value = e.target.value;
+    setPassword(value);
+    setIsPasswordInvalid(!validatePassword(value));
+    setIsPasswordMismatch(value !== confirmPassword);
+  }
+
+  const handleConfirmPasswordChange = (e) => {
+    const value = e.target.value;
+    setConfirmPassword(value);
+    setIsPasswordMismatch(value !== password);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
   
-    if (!validateEmail(username)) {
-      console.error('Invalid email format');
-      return;  // 이메일 형식이 올바르지 않으면 제출 중단
+    if (isUsernameInvalid) {
+      console.error('Invalid username format');
+      return;
     }
   
     try {
@@ -49,15 +84,6 @@ const SignUp = () => {
     }
     console.log({ username, password });
   };
-  
-
-  const validateEmail = (value) => value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i);
-  const isInvalid = useMemo(() => {
-    if (username === "") return false;
-
-    return validateEmail(username) ? false : true;
-  }, [username]);
-
 
   return (
     <div className="flex justify-center items-center h-full">
@@ -71,14 +97,11 @@ const SignUp = () => {
             isRequired
             type="text"
             label="Username"
-            color={isInvalid ? "danger" : "default"}
-            errorMessage={isInvalid && "부적절한 id 형식입니다."}
-            onChange={(e) => {
-              console.log(username)
-              setUsername(e.target.value);
-            }}
+            color={isUsernameInvalid ? "danger" : "default"}
+            onChange={handleUsernameChange}
             placeholder="Username"
-            isInvalid={isInvalid}
+            isInvalid={isUsernameInvalid}
+            status
             classNames={{
               label: "text-black dark:text-white",
               input: [
@@ -90,21 +113,63 @@ const SignUp = () => {
               inputWrapper: [
                 "shadow-xl",
                 "bg-default-200",
-                // "dark:bg-default/60",
                 "dark:hover:bg-default/70",
-                // "group-data-[focused=true]:bg-default-200/50",
-                // "dark:group-data-[focused=true]:bg-default/60",
-                // "!cursor-text",
               ],
             }}/>
+            {/* username 검증 에러 */}
+            {isUsernameInvalid && (
+            <div className="text-red-500 text-sm mt-1">
+              영어 소문자 + 숫자로 이루어진 10 ~ 20글자로 작성해주세요.
+            </div>
+  )}
         </div>
         
-
+        {/* password */}
         <div className="w-3/4 mb-2">
           <Input
             isRequired
             type="password"
             label="password"
+            color="primary"
+            onChange={handlePasswordChange}
+            placeholder="password"
+            status={isPasswordInvalid ? "error" : "default"}
+            classNames={{
+              label: "text-black dark:text-white",
+              input: [
+                
+                "text-black/90 dark:text-white/90",
+                "placeholder:text-default-700 dark:placeholder:text-white",
+              ],
+              innerWrapper: "bg-transparent",
+              inputWrapper: [
+                "shadow-xl",
+                "bg-primary-200/50",
+                "dark:bg-default",
+                "backdrop-blur-xl",
+                "backdrop-saturate-200",
+                "hover:bg-default-200/70",
+                "dark:hover:bg-default/70",
+                "group-data-[focused=true]:bg-default-200/50",
+                "dark:group-data-[focused=true]:bg-default/60",
+                "!cursor-text",
+                
+              ],
+            }}
+          />
+          {isPasswordInvalid && (
+              <div className="text-red-500 text-sm mt-1">
+                비밀번호는 8자 이상이어야 하며, 숫자와 특수문자를 포함해야 합니다.
+              </div>
+          )}
+        </div>
+        
+        {/* 비밀번호 확인용 */}
+        <div className="w-3/4 mb-2">
+          <Input
+            isRequired
+            type="password"
+            label="password confirm"
             color="primary"
             onInput={(e) => setPassword(e.target.value)}
             placeholder="password"
